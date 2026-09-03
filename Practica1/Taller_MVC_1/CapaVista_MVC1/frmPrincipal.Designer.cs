49e934b5e7b0f950823dd55425879b2efd79c8d3
CarlosDCR1 <ccalderonr2@miumg.edu.gt>
2026-09-02 22:04:00 -0600

    Subiendo Capa_Vista

---
+++ Practica1/Taller_MVC_1/CapaVista_MVC1/frmPrincipal.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CapaVista_MVC1
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvConsultaTabla = new System.Windows.Forms.DataGridView();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaTabla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvConsultaTabla
+            // 
+            this.dgvConsultaTabla.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvConsultaTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsultaTabla.Location = new System.Drawing.Point(21, 156);
+            this.dgvConsultaTabla.Name = "dgvConsultaTabla";
+            this.dgvConsultaTabla.RowHeadersWidth = 51;
+            this.dgvConsultaTabla.RowTemplate.Height = 24;
+            this.dgvConsultaTabla.Size = new System.Drawing.Size(1135, 246);
+            this.dgvConsultaTabla.TabIndex = 0;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(785, 39);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(133, 48);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 553);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.dgvConsultaTabla);
+            this.Name = "frmPrincipal";
+            this.Text = "frmPrincipal";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaTabla)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvConsultaTabla;
+        private System.Windows.Forms.Button btnConsultar;
+    }
+}
