c6fbc6ce9d730289ffa0d44fb134b9a7745929cf
CarlosDCR1 <ccalderonr2@miumg.edu.gt>
2026-09-02 21:52:04 -0600

    Subiendo Capa_Controlador

---
+++ Practica1/Taller_MVC_1/CapaControlador_MVC1/Controlador.cs
@@ -0,0 +1,23 @@
+﻿using CapaModelo_MVC1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+using System.Data;
+
+namespace CapaControlador_MVC1
+{
+    public class Controlador
+    {
+       Sentencias sentencias = new Sentencias();
+        public DataTable llenarDgv(string nombreTabla)
+        {
+            OdbcDataAdapter daControlador = sentencias.llenarTbl(nombreTabla);
+            DataTable dtControlador = new DataTable();
+            daControlador.Fill(dtControlador);
+            return dtControlador;
+        }
+    }
+}
