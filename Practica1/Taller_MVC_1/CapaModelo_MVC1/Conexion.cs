c4c64fc49c3195e608e01cf3d6a20fd7b262d4f5
CarlosDCR1 <ccalderonr2@miumg.edu.gt>
2026-09-02 21:57:09 -0600

    Subiendo Capa_Modelo

---
+++ Practica1/Taller_MVC_1/CapaModelo_MVC1/Conexion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo_MVC1
+{
+     class Conexion
+    {
+        public OdbcConnection conexion()
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn=sistema_reparto");
+            try
+            { 
+            conn.Open();
+            }
+            catch (OdbcException) 
+            {
+                    Console.WriteLine("No conecto");
+            }
+                return conn;
+
+        }
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }catch (OdbcException)
+            {
+                Console.WriteLine("No se Desconecto");
+            }
+        }
+    }
+}
