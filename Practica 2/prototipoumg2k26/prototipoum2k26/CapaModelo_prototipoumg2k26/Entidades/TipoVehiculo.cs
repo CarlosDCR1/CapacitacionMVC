3c82ae60ca4d38d9c72486d94658136a4eeb6b7b
CarlosDCR1 <ccalderonr2@miumg.edu.gt>
2026-09-11 17:47:42 -0600

    Subiendo capa Modelo Capacitacion 2 DLLs

---
+++ Practica 2/prototipoumg2k26/prototipoum2k26/CapaModelo_prototipoumg2k26/Entidades/TipoVehiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo_prototipoumg2k26.Entidades
+{
+    public class TipoVehiculo
+    {
+        public int IdPK { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
