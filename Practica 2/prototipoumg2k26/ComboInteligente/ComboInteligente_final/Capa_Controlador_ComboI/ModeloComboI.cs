2c7cc79f95cd39c0bb7b05b21e6f1e1c2cf129a7
CarlosDCR1 <ccalderonr2@miumg.edu.gt>
2026-09-11 17:40:10 -0600

    Subiendo capa controlador capacitacion 2 DLLs

---
+++ Practica 2/prototipoumg2k26/ComboInteligente/ComboInteligente_final/Capa_Controlador_ComboI/ModeloComboI.cs
@@ -0,0 +1,23 @@
+﻿using Capa_Modelo_ComboI.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Controlador_ComboI
+{
+    public class ModeloComboI
+    {
+        RepositorioComboI sentencias = new RepositorioComboI();
+
+
+        public DataTable enviarDatos(string _tabla, string _campo1, string _campo2, bool _filtrarEstado = true)
+        {
+            var dtTabla = sentencias.obtenerDatos(_tabla, _campo1, _campo2, _filtrarEstado);
+            return dtTabla;
+        }
+
+    }
+}
