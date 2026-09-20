e265d05846ae05ecb377c170cf23ff8957286501
CarlosDCR1 <ccalderonr2@miumg.edu.gt>
2026-09-20 12:07:14 -0600

    Subiendo Capacitacion Ayudas

--- Practica 3/prototipoumg2k26/prototipoum2k26/CapaVista_prototipoumg2k26/Formas/FrmTipoVehiculo.Designer.cs
+++ Practica 3/prototipoumg2k26/prototipoum2k26/CapaVista_prototipoumg2k26/Formas/FrmTipoVehiculo.Designer.cs
@@ -182,6 +182,7 @@
             this.btnAyuda.TabIndex = 8;
             this.btnAyuda.Text = "Ayuda";
             this.btnAyuda.UseVisualStyleBackColor = false;
+            this.btnAyuda.Click += new System.EventHandler(this.btnAyuda_Click);
             // 
             // comboI1
             // 
