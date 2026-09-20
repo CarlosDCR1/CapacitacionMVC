9b796359225762af2f39dbdc7cf7a0be891d0c4e
CarlosDCR1 <ccalderonr2@miumg.edu.gt>
2026-09-19 22:58:35 -0600

    Subiendo Creacion de Reporte

---
+++ Practica 3/prototipoumg2k26/prototipoum2k26/CapaControlador_prototipoumg2k26/ModeloTipoVehiculo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaModelo_prototipoumg2k26.Contratos;
+using CapaModelo_prototipoumg2k26.Entidades;
+using CapaModelo_prototipoumg2k26.Repositorios;
+using System.ComponentModel.DataAnnotations;
+
+namespace CapaControlador_prototipoumg2k26
+{
+    public class ModeloTipoVehiculo
+    {
+        private int _idPK;
+        private string _nombre;
+        private string _descripcion;
+        private IRepositorioTipoVehiculo RepositorioTipoVehiculo;
+
+        public EstadoEntidad Estado { private get; set; }
+        private List<ModeloTipoVehiculo> ListaTipoVehiculos;
+
+        public int IdPK { get => _idPK; set => _idPK = value; }
+
+        [Required(ErrorMessage = "El campo nombre es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 2, ErrorMessage = "El nombre debe tener entre 2 y 50 caracteres")]
+        public string Nombre { get => _nombre; set => _nombre = value; }
+
+        [StringLength(maximumLength: 255, ErrorMessage = "La descripcion no puede superar los 255 caracteres")]
+        public string Descripcion { get => _descripcion; set => _descripcion = value; }
+
+        public ModeloTipoVehiculo()
+        {
+            RepositorioTipoVehiculo = new RepositorioTipoVehiculo();
+        }
+
+        public string GrabarCambios()
+        {
+            string mensaje = null;
+            try
+            {
+                var modeloDatosTipoVehiculo = new TipoVehiculo();
+                modeloDatosTipoVehiculo.IdPK = _idPK;
+                modeloDatosTipoVehiculo.Nombre = _nombre;
+                modeloDatosTipoVehiculo.Descripcion = _descripcion;
+                switch (Estado)
+                {
+                    case EstadoEntidad.Added:
+                        RepositorioTipoVehiculo.Agregar(modeloDatosTipoVehiculo);
+                        mensaje = "Grabacion exitosa";
+                        break;
+                    case EstadoEntidad.Modified:
+                        RepositorioTipoVehiculo.Editar(modeloDatosTipoVehiculo);
+                        mensaje = "Actualizacion exitosa";
+                        break;
+                    case EstadoEntidad.Deleted:
+                        RepositorioTipoVehiculo.Remover(modeloDatosTipoVehiculo);
+                        mensaje = "Eliminacion exitosa";
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.ToString();
+            }
+            return mensaje;
+        }
+
+        public List<ModeloTipoVehiculo> GetAll()
+        {
+            var modeloDatosTipoVehiculo = RepositorioTipoVehiculo.GetAll();
+            ListaTipoVehiculos = new List<ModeloTipoVehiculo>();
+            foreach (TipoVehiculo item in modeloDatosTipoVehiculo)
+            {
+                ListaTipoVehiculos.Add(new ModeloTipoVehiculo
+                {
+                    _idPK = item.IdPK,
+                    _nombre = item.Nombre,
+                    _descripcion = item.Descripcion
+                });
+            }
+            return ListaTipoVehiculos;
+        }
+
+        public IEnumerable<ModeloTipoVehiculo> FindbyId(string filter)
+        {
+            return ListaTipoVehiculos.FindAll(t => t.Nombre.Contains(filter));
+        }
+    }
+}
---
+++ Practica 3/prototipoumg2k26/prototipoum2k26/CapaModelo_prototipoumg2k26/Repositorios/RepositorioTipoVehiculo.cs
@@ -0,0 +1,65 @@
+﻿using CapaModelo_prototipoumg2k26.Contratos;
+using CapaModelo_prototipoumg2k26.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo_prototipoumg2k26.Repositorios
+{
+    public class RepositorioTipoVehiculo : RepositorioMaestro, IRepositorioTipoVehiculo
+    {
+        private string selectAll;
+        private string insert;
+        private string update;
+        private string delete;
+        public RepositorioTipoVehiculo()
+        {
+            selectAll = "SELECT * FROM tipo_vehiculo";
+            insert = "INSERT INTO tipo_vehiculo value (NULL, ?, ?)";
+            update = "UPDATE tipo_vehiculo SET nombre_tipo_vehiculo=?, descripcion_tipo_vehiculo=? WHERE id_tipo_vehiculo=?";
+            delete = "DELETE FROM tipo_vehiculo WHERE id_tipo_vehiculo=?";
+        }
+        public int Agregar(TipoVehiculo entidad)
+        {
+            var _parametros = new List<OdbcParameter>();
+            _parametros.Add(new OdbcParameter("p_Nombre", entidad.Nombre));
+            _parametros.Add(new OdbcParameter("p_Descripcion", entidad.Descripcion));
+
+            return EjecucionNonQuery(insert, _parametros, CommandType.Text);
+        }
+        public int Editar(TipoVehiculo entidad)
+        {
+            var _parametros = new List<OdbcParameter>();
+            _parametros.Add(new OdbcParameter("p_Nombre", entidad.Nombre));
+            _parametros.Add(new OdbcParameter("p_Descripcion", entidad.Descripcion));
+            _parametros.Add(new OdbcParameter("p_IdPK", entidad.IdPK));
+            return EjecucionNonQuery(update, _parametros, CommandType.Text);
+        }
+        public int Remover(TipoVehiculo entidad)
+        {
+            var _parametros = new List<OdbcParameter>();
+            _parametros.Add(new OdbcParameter("p_IdPK", entidad.IdPK));
+            return EjecucionNonQuery(delete, _parametros, CommandType.Text);
+        }
+        public IEnumerable<TipoVehiculo> GetAll()
+        {
+            var lstTipoVehiculo = new List<TipoVehiculo>();
+            var tblTabla = EjecucionConsulta(selectAll, CommandType.Text);
+            foreach (DataRow row in tblTabla.Rows)
+            {
+                var tipoVehiculo = new TipoVehiculo();
+                tipoVehiculo.IdPK = Convert.ToInt32(row[0]);
+                tipoVehiculo.Nombre = row[1].ToString();
+                tipoVehiculo.Descripcion = row[2].ToString();
+                lstTipoVehiculo.Add(tipoVehiculo);
+            }
+            tblTabla.Clear();
+            tblTabla = null;
+            return lstTipoVehiculo;
+        }
+    }
+}
